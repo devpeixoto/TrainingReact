20f3c804a0e0197c09f79f83ee7b2105c59c7bdc
Gabriel <peixotogp@hotmail.com>
2022-11-08 18:31:41 -0300

    teste

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './pages/Home'
 import Login from './pages/Login'
 import Admin from './pages/Admin'
 import Error from './pages/Error'
+
 const router = createBrowserRouter([
   {
     path: '/',
