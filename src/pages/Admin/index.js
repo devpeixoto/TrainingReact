0062c1a3780d0fd13c6ba4f87f4b263431d4ed9f
Gabriel <peixotogp@hotmail.com>
2022-11-28 22:50:13 -0300

    Cadastro no banco de dados

--- src/pages/Admin/index.js
+++ src/pages/Admin/index.js
@@ -5,22 +5,71 @@ import { Input } from "../../components/input"
 
 import { MdAddLink} from 'react-icons/md'
 import { FiTrash2 } from 'react-icons/fi'
+import { useState } from 'react'
+import { toast } from 'react-toastify'
+
+import { db } from '../../services/firebaseConnection'
+
+import {
+    addDoc,
+    collection,
+    onSnapshot,
+    query,
+    orderBy,
+    doc,
+    deleteDoc
+} from 'firebase/firestore'
 
 export default function Admin(){
+    
+    const [nameInput, setNameInput] = useState("")
+    const [urlInput, setUrlInput] = useState("")
+    const [backgroundColorInput, setBackgroundColorInput] = useState("#f1f1f1")
+    const [textColorInput, setTextColorInput] = useState("#121212")
+    
+    async function handleRegister(e){
+        e.preventDefault();
+
+        if(nameInput === "" || urlInput ===""){
+            toast.warn("Preencha todos os campos")
+            return;
+        }
+        addDoc(collection(db, "links"), {
+            name:nameInput,
+            url: urlInput,
+            bg: backgroundColorInput,
+            color: textColorInput,
+            created: new Date()
+        })
+        .then(() => {
+            setNameInput("")
+            setUrlInput("")
+            console.log("Link registrado com sucesso")
+        })
+        .catch((error) => {
+            console.log("ERRO AO REGISTRAR" + error)
+            toast.error("ops erro ao salvar link")
+        })
+    }
+
     return(
         <div className="admin-container">
             <Header />
             <Logo />
 
-            <form className='form'>
+            <form className='form' onSubmit={handleRegister}>
                 <label className='label'>Nome do Link</label>
                 <Input 
                     placeholder="Nome do Link..."
+                    value={nameInput}
+                    onChange={ (e) => setNameInput(e.target.value)}
                 />
                 <label className='label'>Url do Link</label>
                 <Input 
                     type="url"
                     placeholder="Digite a url..."
+                    value={urlInput}
+                    onChange={ (e) => setUrlInput(e.target.value)}
                 />
                 
                 <section className='container-colors'>
@@ -28,15 +77,28 @@ export default function Admin(){
                         <label className='label right'>Fundo do link</label>
                         <input 
                             type="color"
+                            value={backgroundColorInput}
+                            onChange={(e) => setBackgroundColorInput(e.target.value)}
                         />
                     </div>
                     <div>
                         <label className='label right'>Cor do link</label>
                         <input 
                             type="color"
+                            value={textColorInput}
+                            onChange={(e) => setTextColorInput(e.target.value)}
                         />
                     </div>
                 </section>
+
+               {nameInput !== '' && (
+                 <div className='preview'>
+                    <label className='label'>Veja como está ficando </label>
+                    <article className='list' style={{marginBottom: 8, marginTop: 7, backgroundColor: backgroundColorInput }}>
+                        <p style={{color: textColorInput}}>{nameInput}</p>
+                    </article>
+                 </div>
+               ) }
 
                 <button className='btn-register' type='submit'>
                     Cadastrar <MdAddLink size={24} color='#fff'/>
@@ -48,10 +110,13 @@ export default function Admin(){
                 Meus Links
             </h2>
 
-            <article className='list'>
+            <article 
+            className='list animate-pop'
+            style={{backgroundColor: "#000", color: "#fff"}}
+            >
                 <p>Grupo Exclusivo no Telegram</p>
                 <div>
-                    <button>
+                    <button className='btn-delete'>
                         <FiTrash2 size={18} color='#fff' />
                     </button>
                 </div>
